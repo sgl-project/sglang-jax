fe5b47796c162ac0a7ccdc451cde98f1441195ab
pc-new <pengchengneo@gmail.com>
2025-08-25 15:58:37 +0800

    fix all gather bug

--- python/sgl_jax/srt/managers/scheduler.py
+++ python/sgl_jax/srt/managers/scheduler.py
@@ -783,10 +783,7 @@ class Scheduler(
         if self.server_args.enable_dp_attention:
             # 执行all gather, 统计信息, 决定当前scheduler是否需要idle batch
             local_batch_size = ret.batch_size if ret is not None else 0
-            logger.info(f"Node {self.node_rank} local batch size: {local_batch_size}")
-            batch_size_list = process_allgather(local_batch_size)
-            logger.info(f"Node {self.node_rank} batch size list: {batch_size_list}")
-            jax.block_until_ready(batch_size_list)
+            batch_size_list = jax.lax.all_gather(local_batch_size, axis_name="data")
             is_idle = all(size == 0 for size in batch_size_list)
             if not is_idle and ret is None:
                 ret = self.get_idle_batch()
