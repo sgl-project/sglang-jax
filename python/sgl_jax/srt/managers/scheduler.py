cecc804b5f6b0cb2e308961360d5feeb7e063a9e
pc-new <pengchengneo@gmail.com>
2025-08-25 19:59:18 +0800

    put data on device

--- python/sgl_jax/srt/managers/scheduler.py
+++ python/sgl_jax/srt/managers/scheduler.py
@@ -808,7 +808,7 @@ class Scheduler(
                 lambda x: jax.lax.all_gather(x, "i"), axis_name="i"
             )(local_batch_size_list)
             logger.info(f"Node {self.node_rank} batch_size_list: {batch_size_list}")
-            is_all_idle = all(size == 0 for size in batch_size_list)
+            is_all_idle = all(size == 0 for size in batch_size_list.flatten())
             if not is_all_idle and ret is None:
                 ret = self.get_idle_batch()
         logger.info(f"after dp sync Node {self.node_rank} ret: {ret}")
