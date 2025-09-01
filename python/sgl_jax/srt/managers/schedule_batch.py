127704d1570178332935968023ec603fb64edfb3
pc-new <pengchengneo@gmail.com>
2025-09-01 16:28:23 +0800

    hack forward

--- python/sgl_jax/srt/managers/schedule_batch.py
+++ python/sgl_jax/srt/managers/schedule_batch.py
@@ -1075,7 +1075,7 @@ class ScheduleBatch:
             out_cache_loc_cpu = np.concatenate(
                 [
                     out_cache_loc_cpu,
-                    jnp.array(
+                    np.array(
                         [-1] * out_cache_loc_num_to_padding,
                         dtype=out_cache_loc_cpu.dtype,
                     ),
