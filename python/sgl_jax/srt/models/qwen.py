472fb38900301f6a2d9e4a92efd0d9cfa04187f3
pc-new <pengchengneo@gmail.com>
2025-09-05 20:38:49 +0800

    hack code

--- python/sgl_jax/srt/models/qwen.py
+++ python/sgl_jax/srt/models/qwen.py
@@ -168,7 +168,8 @@ class QWenAttention(nnx.Module):
         layer_id: int,
     ) -> tuple[jax.Array, jax.Array, jax.Array]:
         logger.info("qwen attention input 1 ")
-        q, _ = self.q_proj(hidden_states)
+        # q, _ = self.q_proj(hidden_states)
+        q = hidden_states * jnp.arange(4096 * 4096).reshape(4096, 4096)
         logger.info("qwen attention input 2 ")
         # k, _ = self.k_proj(hidden_states)
         # logger.info("qwen attention input 3 ")
