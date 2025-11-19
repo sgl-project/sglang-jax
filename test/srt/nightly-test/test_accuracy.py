b87307f1ede0b56984113f5138604bad3a308b33
lzlfwow <2253515@tongji.edu.cn>
2025-11-19 13:34:41 +0800

    nightly-test-accuracy-text-models-1-tpu

--- test/srt/nightly-test/test_accuracy.py
+++ test/srt/nightly-test/test_accuracy.py
@@ -12,6 +12,7 @@ from sgl_jax.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     QWEN_7B,
+    QWEN3_8B,
     CustomTestCase,
     popen_launch_server,
 )
@@ -47,6 +48,8 @@ class TestModelAccuracy(CustomTestCase):
                 "2048",
                 "--tp-size",
                 "1",
+                "--grammar-backend", 
+                "none", 
             ],
             env={
                 "JAX_COMPILATION_CACHE_DIR": "/tmp/jax_compilation_cache",
@@ -67,10 +70,62 @@ class TestModelAccuracy(CustomTestCase):
         ## kill process
         kill_process_tree(process.pid)
 
-    def test_qwen_7b_tp_4(self):
-        pass
 
     def test_qwen3_8b(self):
+        model = QWEN3_8B
+        base_url = DEFAULT_URL_FOR_TEST
+        process = popen_launch_server(
+            QWEN3_8B,
+            DEFAULT_URL_FOR_TEST,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            device="tpu",
+            other_args=[
+                "--trust-remote-code",
+                "--skip-server-warmup",
+                "--random-seed",
+                "3",
+                "--max-prefill-tokens",
+                "16384",
+                "--download-dir",
+                "/dev/shm/",
+                "--dtype",
+                "bfloat16",
+                "--max-running-requests",
+                "256",
+                "--attention-backend",
+                "fa",
+                "--page-size",
+                "128",
+                "--chunked-prefill-size",
+                "2048",
+                "--tp-size",
+                "1",
+                "--grammar-backend", 
+                "none", 
+            ],
+            env={
+                "JAX_COMPILATION_CACHE_DIR": "/tmp/jax_compilation_cache",
+            },
+        )
+        ## test mmlu
+        args = SimpleNamespace(
+            base_url=base_url,
+            model=model,
+            eval_name="mmlu",
+            num_examples=256,
+            num_threads=128,
+            max_tokens=1024,
+        )
+        metrics = run_eval(args)
+        
+
+        self.assertGreater(metrics["score"], 0.5)
+
+        ## kill process
+        kill_process_tree(process.pid)
+
+
+    def test_qwen_7b_tp_4(self):
         pass
 
     def test_qwen3_8b_tp_4(self):
