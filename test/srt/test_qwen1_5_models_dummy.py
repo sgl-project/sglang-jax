ce5dccef7038dd81f305357ea6121565c9f25652
pathfinder-pf <13222202032@163.com>
2025-11-19 21:03:39 +0800

    add tunix sampler test (#454)
    Co-authored-by: pathfinder-fp <slackexplorer@gmail.com>

---
+++ test/srt/test_qwen1_5_models_dummy.py
@@ -0,0 +1,66 @@
+import unittest
+from types import SimpleNamespace
+
+from run_eval import run_eval
+
+from sgl_jax.srt.utils import kill_process_tree
+from sgl_jax.test.test_utils import (
+    DEEPSEEK_R1_QWEN_1_5B,
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server,
+)
+
+
+class TestQwen1_5Model(CustomTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = DEEPSEEK_R1_QWEN_1_5B
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            device="tpu",
+            other_args=[
+                "--trust-remote-code",
+                "--skip-server-warmup",
+                "--random-seed",
+                "3",
+                "--max-prefill-tokens",
+                "16384",
+                "--download-dir",
+                "/dev/shm/",
+                "--dtype",
+                "bfloat16",
+                "--max-running-requests",
+                "256",
+                "--attention-backend",
+                "fa",
+                "--page-size",
+                "128",
+                "--chunked-prefill-size",
+                "2048",
+                "--precompile-bs-paddings",
+                "64",
+                "--precompile-token-paddings",
+                "8192",
+                "--load-format",
+                "dummy",
+            ],
+            env={
+                "JAX_COMPILATION_CACHE_DIR": "/tmp/jax_compilation_cache",
+            },
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_skip(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
