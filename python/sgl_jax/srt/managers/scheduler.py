7b7db648c9666b8cdc7bef47a4322f1a3e568085
aolemila <aolemila@primatrix.ai>
2025-12-02 15:31:32 +0800

    add --enable-static-lora and --lora-scaling (#497)

--- python/sgl_jax/srt/managers/scheduler.py
+++ python/sgl_jax/srt/managers/scheduler.py
@@ -1194,6 +1194,7 @@ class Scheduler(
                 precompile_bs_paddings,
                 precompile_cache_loc_paddings,
                 self.page_size,
+                self.server_args.enable_static_lora,
             )
 
             if self.enable_overlap:
@@ -1234,6 +1235,7 @@ class Scheduler(
                 precompile_bs_paddings,
                 precompile_cache_loc_paddings,
                 self.page_size,
+                self.server_args.enable_static_lora,
                 # eagle's model_worker_batch will be modified and repadding within eagle_worker
                 skip_padding=True,
             )
