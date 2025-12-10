312452f3d632884d16132b43070b8f62ebc8a94a
Weinan Liu <weinanliu@sii.edu.cn>
2025-12-10 17:48:01 +0000

    GPT-OSS: New empty

---
+++ python/sgl_jax/srt/models/gpt_oss.py
@@ -0,0 +1,49 @@
+import logging
+from typing import Any
+
+import jax
+import jax.numpy as jnp
+from flax import nnx
+from transformers import PretrainedConfig
+
+from sgl_jax.srt.configs.model_config import ModelConfig
+from sgl_jax.srt.layers.embeddings import Embed, ParallelLMHead, RotaryEmbedding
+from sgl_jax.srt.layers.layernorm import RMSNorm
+from sgl_jax.srt.layers.linear import LinearBase
+from sgl_jax.srt.layers.logits_processor import LogitsMetadata, LogitsProcessor
+from sgl_jax.srt.layers.radix_attention import RadixAttention
+from sgl_jax.srt.mem_cache.memory_pool import KVCache
+from sgl_jax.srt.model_executor.forward_batch_info import ForwardBatch
+from sgl_jax.srt.precision_tracer import precision_tracer
+from sgl_jax.srt.utils.weight_utils import WeightLoader, WeightMapping
+
+logger = logging.getLogger(__name__)
+
+init_fn = nnx.initializers.uniform()
+
+
+class GptOssForCausalLM(nnx.Module):
+    def __init__(
+        self,
+        config: PretrainedConfig,
+        mesh: jax.sharding.Mesh,
+        dtype: jnp.dtype = jnp.bfloat16,
+    ):
+        self.mesh = mesh
+        self.config = config
+        self.dtype = dtype
+        logger.info("GptOssForCausalLM config dtype: %s", self.dtype)
+
+    def load_weights(self, model_config: ModelConfig):
+        raise NotImplementedError
+
+    def __call__(
+        self,
+        forward_batch: ForwardBatch,
+        token_to_kv_pool: KVCache,
+        logits_metadata: LogitsMetadata,
+    ):
+        raise NotImplementedError
+
+
+EntryClass = GptOssForCausalLM
