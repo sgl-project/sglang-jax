f24e44833609881b6a0ddbbe209fa21f8352b4d9
Bob Chen <70640928+Bob-Chen222@users.noreply.github.com>
2025-12-11 20:20:15 +0800

    init with quantization utils

--- python/sgl_jax/srt/model_executor/model_runner.py
+++ python/sgl_jax/srt/model_executor/model_runner.py
@@ -135,6 +135,7 @@ class ModelRunner:
         # Load the model
         self.sampler = Sampler(nnx.Rngs(server_args.random_seed), mesh=self.mesh)
         total_device_memory = self.get_available_device_memory()
+        self.init_attention_backend()
         self.load_model()
         if not self.is_draft_worker:
             self.initialize_jit()
@@ -154,7 +155,6 @@ class ModelRunner:
             total_device_memory,
         )
 
-        self.init_attention_backend()
 
     def initialize_jit(self):
         model_def, model_state = nnx.split(self.model)
@@ -241,9 +241,11 @@ class ModelRunner:
         self.model_config.configure_for_tensor_parallel(self.tp_size)
         self.model_config.log_kv_heads_info(self.tp_size)
         self.model_config.hf_config.ep_size = self.ep_size
+        
 
         self.model = self.model_loader.load_model(
             model_config=self.model_config,
+            attn_backend=self.attn_backend,
         )
 
         # Parse other args
