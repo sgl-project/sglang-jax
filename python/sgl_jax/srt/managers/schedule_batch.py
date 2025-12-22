07483ab20a814affea79c6aeb9badf7ab437b3a4
Brian <brian@primatrix.ai>
2025-12-22 19:09:09 +0800

    feat: radix cache with dp (#588)

--- python/sgl_jax/srt/managers/schedule_batch.py
+++ python/sgl_jax/srt/managers/schedule_batch.py
@@ -162,6 +162,7 @@ class Req:
         token_ids_logprob: list[int] = None,
         stream: bool = False,
         extra_key: str | None = None,
+        dp_rank: int | None = None,
         origin_input_ids_unpadded: tuple[int] | None = None,
         eos_token_ids: set[int] | None = None,
         vocab_size: int | None = None,
@@ -186,6 +187,7 @@ class Req:
         self.return_hidden_states = return_hidden_states
 
         self.extra_key = extra_key
+        self.dp_rank = dp_rank
 
         # Memory pool info
         self.req_pool_idx: int | None = None
@@ -345,7 +347,7 @@ class Req:
                 self.last_host_node,
                 self.host_hit_length,
             ) = tree_cache.match_prefix(
-                key=RadixKey(self.adjust_max_prefix_ids(), self.extra_key),
+                key=RadixKey(self.adjust_max_prefix_ids(), self.extra_key, self.dp_rank),
             )
             self.last_matched_prefix_len = len(self.prefix_indices)
         self.extend_input_len = len(self.fill_ids) - len(self.prefix_indices)
