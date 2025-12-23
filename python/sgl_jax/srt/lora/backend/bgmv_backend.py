3a306899e38b7ac3aa1c10bb624b189e5c2c0bef
aolemila <aolemilaluo@gmail.com>
2025-12-23 15:16:22 +0800

    update copyright

--- python/sgl_jax/srt/lora/backend/bgmv_backend.py
+++ python/sgl_jax/srt/lora/backend/bgmv_backend.py
@@ -1,3 +1,20 @@
+# Adapted from https://github.com/vllm-project/tpu-inference/blob/v0.11.1/tpu_inference/lora/torch_lora_ops.py
+# Copyright 2025 The tpu-inference Authors. All rights reserved.
+
+# Copyright 2024 The T5X Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import jax
 import jax.numpy as jnp
 import numpy as np
