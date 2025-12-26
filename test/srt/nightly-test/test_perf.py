cec4cde4cdd809e16c732279e13571e507a3ff10
aolemila <aolemilaluo@gmail.com>
2025-12-26 15:12:09 +0800

    fix nightly-ci test_qwen3_32B_lora_r32_performance_tp_4

--- test/srt/nightly-test/test_perf.py
+++ test/srt/nightly-test/test_perf.py
@@ -1599,7 +1599,7 @@ class TestModePerf(CustomTestCase):
     def test_qwen3_32B_lora_r32_performance_tp_4(self):
         model = "Qwen/Qwen3-32B"
         lora_path = "flyfishxu/DeepNews-LoRA-Qwen3-32B"
-        lora_name = "DeepNews-LoRA-Qwen3-32B"
+        lora_name = ["DeepNews-LoRA-Qwen3-32B"]
         base_url = DEFAULT_URL_FOR_TEST
 
         allow_gap = 0.03
@@ -1662,13 +1662,8 @@ class TestModePerf(CustomTestCase):
 
         # define test parameters
         # concurrency levels
-        concurrency_levels = [8, 16, 32, 64, 128, 256]
-        # input length levels (1k, 4k, 8k)
+        concurrency_levels = [1, 8, 16, 32, 64, 128, 256]
         input_lengths = [4096]
-
-        # concurrency_levels = [8]
-        # # input length levels (1k, 4k, 8k)
-        # input_lengths = [1024]
 
         output_lengths = [1, 1024]
         specific_args = self.BASIC_SERVER_ARGS + [
@@ -1679,6 +1674,10 @@ class TestModePerf(CustomTestCase):
             "--disable-radix-cache",
             "--lora-paths",
             lora_path,
+            "--precompile-token-paddings",
+            "2048",
+            "--precompile-bs-paddings",
+            "256",
         ]
         # launch server
         process = popen_launch_server(
@@ -1706,22 +1705,51 @@ class TestModePerf(CustomTestCase):
 
                         num_prompts = concurrency * 3
 
-                        args = get_benchmark_args(
-                            base_url=self.base_url,
+                        args = argparse.Namespace(
+                            backend="sglang-oai",
+                            base_url=base_url,
+                            host="127.0.0.1",
+                            port=int(base_url.split(":")[-1]),
                             dataset_name="random",
+                            dataset_path="",
+                            model=None,
+                            tokenizer=None,
                             num_prompts=num_prompts,
-                            request_rate=float("inf"),
+                            sharegpt_output_len=None,
+                            sharegpt_context_len=None,
                             random_input_len=in_len,
                             random_output_len=out_len,
+                            random_range_ratio=1.0,
+                            request_rate=float("inf"),
                             max_concurrency=concurrency,
-                            random_range_ratio=1,
+                            output_file=None,
+                            output_details=False,
+                            disable_tqdm=False,
+                            disable_stream=False,
+                            return_logprob=False,
+                            seed=1,
                             disable_ignore_eos=True,
+                            extra_request_body=None,
+                            apply_chat_template=False,
+                            profile=False,
                             lora_name=lora_name,
-                            backend="sglang-oai",
+                            prompt_suffix="",
+                            pd_separated=False,
+                            flush_cache=False,
                             warmup_requests=0,
+                            tokenize_prompt=False,
+                            adjust_prompt_max_retry=10,
+                            gsp_num_groups=64,
+                            gsp_prompts_per_group=16,
+                            gsp_system_prompt_len=2048,
+                            gsp_question_len=128,
+                            gsp_output_len=256,
                         )
 
                         metrics = run_benchmark(args)
+
+                        if concurrency == 1:
+                            continue
 
                         if out_len == 1:
                             self.assertGreater(
