94029e62ef9db9f35f1c443a7f2885a1ac991f0a
lzlfwow <2253515@tongji.edu.cn>
2025-12-31 10:09:02 +0800

    fix pref trace tpu1 timeout

--- test/srt/nightly-test/test_perf_trace.py
+++ test/srt/nightly-test/test_perf_trace.py
@@ -1254,117 +1254,63 @@ class TestModelPerfTrace(CustomTestCase):
 
                 writer.writerows(results_summary)
 
-    def test_qwen3_32B_lora_r32_performance_trace_tp_4(self):
-        model = "Qwen/Qwen3-32B"
-        lora_path = "flyfishxu/DeepNews-LoRA-Qwen3-32B"
-        lora_name = ["DeepNews-LoRA-Qwen3-32B"]
-        base_url = DEFAULT_URL_FOR_TEST
+    def test_QWEN2_5_7B_INSTRUCT_performance_trace_tp_4(self):
+        import os
 
-        allow_gap = 0.03
-        # max_concurrency:input_seq_len:ttft|itl|input_throughput|output_throughput
-        # Note: ttft is median_ttft_ms, itl is median_itl_ms
-        # Baseline:
-        # - branch: epic/support-multi-lora
-        # - commit: Fix/mismatch len lora (#573)
-        # See more metrics from gcloud storage bucket: perf_data_tmp/sglang_jax_lora/epic_lora_09f5dab3f9c_align_test_perf_launch.tar
-        expected_performance = {
-            8: {
-                4096: {
-                    "ttft": 1663.94,
-                    "itl": 19.37,
-                    "input_throughput": 14414.54,
-                    "output_throughput": 377.76,
-                }
-            },
-            16: {
-                4096: {
-                    "ttft": 3327.81,
-                    "itl": 21.76,
-                    "input_throughput": 19633.36,
-                    "output_throughput": 651.62,
-                }
-            },
-            32: {
-                4096: {
-                    "ttft": 6654.10,
-                    "itl": 24.67,
-                    "input_throughput": 19647.09,
-                    "output_throughput": 843.85,
-                }
-            },
-            64: {
-                4096: {
-                    "ttft": 13301.45,
-                    "itl": 24.72,
-                    "input_throughput": 19677.24,
-                    "output_throughput": 845.64,
-                }
-            },
-            128: {
-                4096: {
-                    "ttft": 26595.13,
-                    "itl": 24.81,
-                    "input_throughput": 19691.40,
-                    "output_throughput": 845.10,
-                }
-            },
-            256: {
-                4096: {
-                    "ttft": 53191.76,
-                    "itl": 24.78,
-                    "input_throughput": 19697.55,
-                    "output_throughput": 860.46,
-                }
-            },
-        }
+        MOUNT_ROOT = os.getenv("CI_MOUNT_ROOT", "/models")
+        raw_model_id = QWEN2_5_7B_INSTRUCT
+        model_dir_name = "QWEN2_5_7B_INSTRUCT"
+        cached_model_path = os.path.join(MOUNT_ROOT, "model_scope", model_dir_name)
+
+        print(f"[CI Info] Checking Model Cache at: {cached_model_path}")
+        if os.path.exists(cached_model_path):
+            print(f"[CI Info] Hit Model Cache: {cached_model_path}")
+            model_path_for_server = cached_model_path
+        else:
+            print(f"[CI Info] Cache Miss, downloading: {raw_model_id}")
+            model_path_for_server = raw_model_id
+
+        base_url = DEFAULT_URL_FOR_TEST
 
         # define test parameters
         # concurrency levels
         concurrency_levels = [8, 16, 32, 64, 128, 256]
-        input_lengths = [4096]
+        # input length levels (1k, 4k, 8k)
+        input_lengths = [1024, 4096, 8192]
+
+        # concurrency_levels = [8]
+        # # input length levels (1k, 4k, 8k)
+        # input_lengths = [1024]
 
         output_lengths = [1, 16]
         specific_args = self.BASIC_SERVER_ARGS + [
             "--tp-size",
             "4",
-            "--device",
-            "tpu",
-            "--disable-radix-cache",
-            "--lora-paths",
-            lora_path,
         ]
         # launch server
         process = popen_launch_server(
-            model,
+            model_path_for_server,
             base_url,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             device="tpu",
             other_args=specific_args,
             env={
                 "JAX_COMPILATION_CACHE_DIR": "/tmp/jax_compilation_cache",
-                "SGLANG_JAX_PROFILER_DIR": trace_output_dir + "/" + model + self.TP_4,
+                "SGLANG_JAX_PROFILER_DIR": trace_output_dir + "/" + raw_model_id + self.TP_4,
             },
         )
 
         results_summary = []
 
-        # warmup to load weight
-        args = get_benchmark_args(
-            base_url=base_url,
-            dataset_name="random",
-            num_prompts=1,
-            request_rate=float("inf"),
-            random_input_len=1024,
-            random_output_len=1,
-            max_concurrency=1,
-            random_range_ratio=1,
-            lora_name=lora_name,
-            backend="sglang-oai",
-            warmup_requests=0,
-        )
-        run_benchmark(args)
-
         try:
+            static_config = {
+                "dataset_name": "random",
+                "dataset_path": self.sharegpt_dataset_path,
+                "warmup_requests": 0,
+                "profile": True,
+                "num_steps": 10,
+            }
+
             for concurrency in concurrency_levels:
                 for in_len in input_lengths:
                     for out_len in output_lengths:
@@ -1374,47 +1320,24 @@ class TestModelPerfTrace(CustomTestCase):
                         )
                         print(f"{'#'*60}")
 
-                        num_prompts = concurrency * 3
-                        static_config = {
-                            "profile": True,
-                            "num_steps": 10,
-                        }
+                        current_num_prompts = concurrency * 3
+
                         args = get_benchmark_args(
                             base_url=base_url,
-                            dataset_name="random",
-                            num_prompts=num_prompts,
-                            request_rate=float("inf"),
+                            tokenizer=model_path_for_server,
+                            num_prompts=current_num_prompts,
+                            max_concurrency=concurrency,
                             random_input_len=in_len,
                             random_output_len=out_len,
-                            max_concurrency=concurrency,
-                            random_range_ratio=1,
-                            lora_name=lora_name,
-                            backend="sglang-oai",
-                            warmup_requests=0,
+                            random_range_ratio=1.0,
+                            request_rate=float("inf"),
+                            seed=42,
                         )
+
                         vars(args).update(static_config)
+
                         metrics = run_benchmark(args)
 
-                        if out_len == 1:
-                            self.assertGreater(
-                                metrics["input_throughput"],
-                                expected_performance[concurrency][in_len]["input_throughput"]
-                                * (1 - allow_gap),
-                            )
-                            self.assertLess(
-                                metrics["median_ttft_ms"],
-                                expected_performance[concurrency][in_len]["ttft"] * (1 + allow_gap),
-                            )
-                        else:
-                            self.assertGreater(
-                                metrics["output_throughput"],
-                                expected_performance[concurrency][in_len]["output_throughput"]
-                                * (1 - allow_gap),
-                            )
-                            self.assertLess(
-                                metrics["median_itl_ms"],
-                                expected_performance[concurrency][in_len]["itl"] * (1 + allow_gap),
-                            )
                         results_summary.append(
                             {
                                 "concurrency": concurrency,
@@ -1424,7 +1347,7 @@ class TestModelPerfTrace(CustomTestCase):
                                 "itl_ms": metrics.get("median_itl_ms", 0),
                                 "in_tps": metrics.get("input_throughput", 0),
                                 "out_tps": metrics.get("output_throughput", 0),
-                                "model_name": model,
+                                "model_name": raw_model_id,
                                 "tpu_size": 4,
                             }
                         )
@@ -1453,47 +1376,36 @@ class TestModelPerfTrace(CustomTestCase):
 
                 writer.writerows(results_summary)
 
-        print("\n" + "=" * 100)
-        print(
-            f"{'Con':<5} | {'Input':<6} | {'Output':<6} | {'TTFT (ms)':<10} | {'ITL (ms)':<10} | {'Out TPS':<10} | {'In TPS':<10}| {'Model':<20}"
-        )
-        print("-" * 100)
-        for r in results_summary:
-            print(
-                f"{r['concurrency']:<5} | {r['input']:<6} | {r['output']:<6} | {r['ttft_ms']:<10.2f} | {r['itl_ms']:<10.2f} | {r['out_tps']:<10.2f} | {r['in_tps']:<10.2f}| {r['model_name']:<20}"
-            )
-        print("=" * 100)
-
     def test_qwen_7b_performance_trace_tp_1_low_concurrency(self):
-        concurrency_levels = [8, 16, 32, 64]
+        concurrency_levels = [8, 16, 32, 64, 128]
         self._test_qwen_7b_performance_trace_tp_1(concurrency_levels)
 
     def test_qwen_7b_performance_trace_tp_1_high_concurrency(self):
-        concurrency_levels = [128, 256]
+        concurrency_levels = [256]
         self._test_qwen_7b_performance_trace_tp_1(concurrency_levels)
 
     def test_qwen3_8b_performance_trace_tp_1_low_concurrency(self):
-        concurrency_levels = [8, 16, 32, 64]
+        concurrency_levels = [8, 16, 32, 64, 128]
         self._test_qwen3_8b_performance_trace_tp_1(concurrency_levels)
 
     def test_qwen3_8b_performance_trace_tp_1_high_concurrency(self):
-        concurrency_levels = [128, 256]
+        concurrency_levels = [256]
         self._test_qwen3_8b_performance_trace_tp_1(concurrency_levels)
 
     def test_GEMMA2_2B_IT_performance_trace_tp_1_low_concurrency(self):
-        concurrency_levels = [8, 16, 32, 64]
+        concurrency_levels = [8, 16, 32, 64, 128]
         self._test_GEMMA2_2B_IT_performance_trace_tp_1(concurrency_levels)
 
     def test_GEMMA2_2B_IT_performance_trace_tp_1_high_concurrency(self):
-        concurrency_levels = [128, 256]
+        concurrency_levels = [256]
         self._test_GEMMA2_2B_IT_performance_trace_tp_1(concurrency_levels)
 
     def test_QWEN2_5_7B_INSTRUCT_performance_trace_tp_1_low_concurrency(self):
-        concurrency_levels = [8, 16, 32, 64]
+        concurrency_levels = [8, 16, 32, 64, 128]
         self._test_QWEN2_5_7B_INSTRUCT_performance_trace_tp_1(concurrency_levels)
 
     def test_QWEN2_5_7B_INSTRUCT_performance_trace_tp_1_high_concurrency(self):
-        concurrency_levels = [128, 256]
+        concurrency_levels = [256]
         self._test_QWEN2_5_7B_INSTRUCT_performance_trace_tp_1(concurrency_levels)
 
 
