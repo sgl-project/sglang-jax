3d457d569c37dd09a2826cef957ed1f26b406068
jimoosciuc <lynn@primatrix.ai>
2026-01-06 16:29:35 +0800

    fix: tuned block configs

--- python/sgl_jax/srt/kernels/fused_moe/v1/kernel.py
+++ python/sgl_jax/srt/kernels/fused_moe/v1/kernel.py
@@ -661,20 +661,32 @@ def _fused_ep_moe_kernel(
                 send_sz += remote_sz
                 expert_offsets_x2_smem[bt_sem_id, 0, e_id] = offset + local_sz + remote_sz
                 start = expert_starts_x2_smem[bt_sem_id, 0, e_id] + offset
-                # TODO(jevinjiang): compare the perf when using branches.
-                pltpu.make_async_copy(
-                    src_ref=tokens_hbm.at[pl.ds(src_t_id, local_sz)],
-                    dst_ref=a2a_s_x2_hbm.at[e_sem_id, pl.ds(start, local_sz)],
-                    sem=recv_x2_sems.at[e_sem_id],
-                ).start()
-                pltpu.make_async_remote_copy(
-                    src_ref=tokens_hbm.at[pl.ds(src_t_id, remote_sz)],
-                    dst_ref=a2a_s_x2_hbm.at[e_sem_id, pl.ds(start, remote_sz)],
-                    send_sem=send_x2_sems.at[e_sem_id],
-                    recv_sem=recv_x2_sems.at[e_sem_id],
-                    device_id=get_mesh_device_id(recv_id),
-                    device_id_type=pltpu.DeviceIdType.MESH,
-                ).start()
+
+                @pl.when(local_sz > 0)
+                def _(local_sz=local_sz, start=start, src_t_id=src_t_id, e_sem_id=e_sem_id):
+                    pltpu.make_async_copy(
+                        src_ref=tokens_hbm.at[pl.ds(src_t_id, local_sz)],
+                        dst_ref=a2a_s_x2_hbm.at[e_sem_id, pl.ds(start, local_sz)],
+                        sem=recv_x2_sems.at[e_sem_id],
+                    ).start()
+
+                @pl.when(remote_sz > 0)
+                def _(
+                    remote_sz=remote_sz,
+                    start=start,
+                    src_t_id=src_t_id,
+                    e_sem_id=e_sem_id,
+                    recv_id=recv_id,
+                ):
+                    pltpu.make_async_remote_copy(
+                        src_ref=tokens_hbm.at[pl.ds(src_t_id, remote_sz)],
+                        dst_ref=a2a_s_x2_hbm.at[e_sem_id, pl.ds(start, remote_sz)],
+                        send_sem=send_x2_sems.at[e_sem_id],
+                        recv_sem=recv_x2_sems.at[e_sem_id],
+                        device_id=get_mesh_device_id(recv_id),
+                        device_id_type=pltpu.DeviceIdType.MESH,
+                    ).start()
+
             return send_sz
 
         send_sz = lax.fori_loop(
@@ -712,19 +724,32 @@ def _fused_ep_moe_kernel(
             is_local = recv_id == my_id
             local_sz = lax.select(is_local, sz, 0)
             remote_sz = lax.select(is_local, 0, sz)
-            pltpu.make_async_copy(
-                src_ref=src_ref.at[e_sem_id, pl.ds(start, local_sz)],
-                dst_ref=a2a_g_hbm.at[my_e_id, pl.ds(0, local_sz)],
-                sem=a2a_gather_sem,
-            ).start()
-            pltpu.make_async_remote_copy(
-                src_ref=src_ref.at[e_sem_id, pl.ds(start, remote_sz)],
-                dst_ref=a2a_g_hbm.at[my_e_id, pl.ds(0, remote_sz)],
-                send_sem=send_x2_sems.at[e_sem_id],
-                recv_sem=a2a_gather_sem,
-                device_id=get_mesh_device_id(recv_id),
-                device_id_type=pltpu.DeviceIdType.MESH,
-            ).start()
+
+            @pl.when(local_sz > 0)
+            def _(start=start, local_sz=local_sz, e_sem_id=e_sem_id, my_e_id=my_e_id):
+                pltpu.make_async_copy(
+                    src_ref=src_ref.at[e_sem_id, pl.ds(start, local_sz)],
+                    dst_ref=a2a_g_hbm.at[my_e_id, pl.ds(0, local_sz)],
+                    sem=a2a_gather_sem,
+                ).start()
+
+            @pl.when(remote_sz > 0)
+            def _(
+                start=start,
+                remote_sz=remote_sz,
+                e_sem_id=e_sem_id,
+                my_e_id=my_e_id,
+                recv_id=recv_id,
+            ):
+                pltpu.make_async_remote_copy(
+                    src_ref=src_ref.at[e_sem_id, pl.ds(start, remote_sz)],
+                    dst_ref=a2a_g_hbm.at[my_e_id, pl.ds(0, remote_sz)],
+                    send_sem=send_x2_sems.at[e_sem_id],
+                    recv_sem=a2a_gather_sem,
+                    device_id=get_mesh_device_id(recv_id),
+                    device_id_type=pltpu.DeviceIdType.MESH,
+                ).start()
+
             start += sz
 
     def wait_a2a_gather_send(*, bt_sem_id, e_sem_id, local_e_id):
