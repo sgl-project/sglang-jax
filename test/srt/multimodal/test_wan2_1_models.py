d1bedd9bc0751222e418a693129f654b1c6d3f2d
neo <pengchengneo@gmail.com>
2026-01-14 20:38:42 +0800

    fix lint

--- test/srt/multimodal/test_wan2_1_models.py
+++ test/srt/multimodal/test_wan2_1_models.py
@@ -1,6 +1,7 @@
-import unittest
-import requests
 import json
+import unittest
+
+import requests
 
 from sgl_jax.srt.utils import kill_process_tree
 from sgl_jax.test.test_utils import (
@@ -12,9 +13,7 @@ from sgl_jax.test.test_utils import (
     popen_launch_server,
 )
 
-headers = {
-    "Content-Type": "application/json"
-}
+headers = {"Content-Type": "application/json"}
 
 
 class TestWan2_1Model(CustomTestCase):
@@ -22,66 +21,68 @@ class TestWan2_1Model(CustomTestCase):
         process = popen_launch_server(
             "/models/Wan-AI/Wan2.1-T2V-1.3B-Diffusers/",
             DEFAULT_URL_FOR_TEST,
-            timeout = DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            device = "tpu",
-            other_args = [
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            device="tpu",
+            other_args=[
                 "--trust-remote-code",
                 "--skip-server-warmup",
                 "--random-seed",
                 "3",
                 "--multimodal",
-                "--disable-precompile"
+                "--disable-precompile",
             ],
-            env = {
+            env={
                 "JAX_COMPILATION_CACHE_DIR": "/tmp/jax_compilation_cache",
             },
-            multimodal = True,
+            multimodal=True,
         )
-        data = {
-            "prompt": "A curious raccoon",
-            "size": "480*832",
-            "num_frames": 41
-        }
-        response = requests.post(DEFAULT_URL_FOR_TEST + "/api/v1/videos/generation", headers = headers, json = data, timeout=1200)
+        data = {"prompt": "A curious raccoon", "size": "480*832", "num_frames": 41}
+        response = requests.post(
+            DEFAULT_URL_FOR_TEST + "/api/v1/videos/generation",
+            headers=headers,
+            json=data,
+            timeout=1200,
+        )
         response.raise_for_status()
         # 解析返回的 JSON 数据
         result = response.json()
         print("请求成功！")
-        print(json.dumps(result, indent = 4, ensure_ascii = False))
+        print(json.dumps(result, indent=4, ensure_ascii=False))
         process.kill()
-
 
     def test_wan2_1_14b(self):
         process = popen_launch_server(
             "/models/Wan-AI/Wan2.1-T2V-14B-Diffusers/",
             DEFAULT_URL_FOR_TEST,
-            timeout = DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            device = "tpu",
-            other_args = [
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            device="tpu",
+            other_args=[
                 "--trust-remote-code",
                 "--skip-server-warmup",
                 "--random-seed",
                 "3",
                 "--multimodal",
-                "--disable-precompile"
+                "--disable-precompile",
             ],
-            env = {
+            env={
                 "JAX_COMPILATION_CACHE_DIR": "/tmp/jax_compilation_cache",
             },
-            multimodal = True,
+            multimodal=True,
         )
-        data = {
-            "prompt": "A curious raccoon",
-            "size": "480*832",
-            "num_frames": 5
-        }
-        response = requests.post(DEFAULT_URL_FOR_TEST + "/api/v1/videos/generation", headers = headers, json = data, timeout=1200)
+        data = {"prompt": "A curious raccoon", "size": "480*832", "num_frames": 5}
+        response = requests.post(
+            DEFAULT_URL_FOR_TEST + "/api/v1/videos/generation",
+            headers=headers,
+            json=data,
+            timeout=1200,
+        )
         response.raise_for_status()
         # 解析返回的 JSON 数据
         result = response.json()
         print("请求成功！")
-        print(json.dumps(result, indent = 4, ensure_ascii = False))
+        print(json.dumps(result, indent=4, ensure_ascii=False))
         process.kill()
+
 
 if __name__ == "__main__":
     unittest.main()
