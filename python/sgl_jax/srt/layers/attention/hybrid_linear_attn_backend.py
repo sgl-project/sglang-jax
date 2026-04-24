42b1a0d9e5a9b1f889f0cdcd68ceae58f4966d3b
aolemila <aolemilaluo@gmail.com>
2026-04-24 20:54:16 +0800

    put the LinearRecurrentAttnXXX into right files

--- python/sgl_jax/srt/layers/attention/hybrid_linear_attn_backend.py
+++ python/sgl_jax/srt/layers/attention/hybrid_linear_attn_backend.py
@@ -1,5 +1,13 @@
 from sgl_jax.srt.layers.attention.base_attn_backend import AttentionBackend
 
 
+class LinearRecurrentAttnBackendMetadata:
+    pass
+
+
+class LinearRecurrentAttnBackend(AttentionBackend):
+    pass
+
+
 class HybridLinearAttnBackend(AttentionBackend):
     pass
