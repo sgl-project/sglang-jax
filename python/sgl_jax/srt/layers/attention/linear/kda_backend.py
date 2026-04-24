9de6cbb8a16d52434c98acbeb1a7d0fd7137891a
aolemila <aolemila@primatrix.ai>
2026-04-24 20:48:01 +0800

    [Tiny Feat] add part files and dir structure (#961)
    * add part files and dir structure

---
+++ python/sgl_jax/srt/layers/attention/linear/kda_backend.py
@@ -0,0 +1,17 @@
+from python.sgl_jax.srt.layers.attention.base_attn_backend import AttentionBackend
+
+
+class LinearRecurrentAttnBackendMetadata:
+    pass
+
+
+class LinearRecurrentAttnBackend(AttentionBackend):
+    pass
+
+
+class KDAAttnBackendMetadata:
+    pass
+
+
+class KDAAttnBackend(LinearRecurrentAttnBackend):
+    pass
