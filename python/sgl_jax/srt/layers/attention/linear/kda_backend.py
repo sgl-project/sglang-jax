f7abb732de7909b7a48f395d5c770f8fa464ea2e
aolemila <aolemila@primatrix.ai>
2026-04-24 20:58:39 +0800

    put the LinearRecurrentAttnXXX into right files (#962)

--- python/sgl_jax/srt/layers/attention/linear/kda_backend.py
+++ python/sgl_jax/srt/layers/attention/linear/kda_backend.py
@@ -1,12 +1,6 @@
-from python.sgl_jax.srt.layers.attention.base_attn_backend import AttentionBackend
-
-
-class LinearRecurrentAttnBackendMetadata:
-    pass
-
-
-class LinearRecurrentAttnBackend(AttentionBackend):
-    pass
+from python.sgl_jax.srt.layers.attention.hybrid_linear_attn_backend import (
+    LinearRecurrentAttnBackend,
+)
 
 
 class KDAAttnBackendMetadata:
