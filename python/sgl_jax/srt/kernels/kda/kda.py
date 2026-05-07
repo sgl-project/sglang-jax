f7ac50018cc05b8da95aadb2807010448580b9d8
pathfinder-pf <slackexplorer@gmail.com>
2026-05-07 21:11:59 +0800

    fix pallas-kernel (#1026)
    * fix pallas-kernel
    
    * fix
    
    ---------
    
    Co-authored-by: whz <wuhuanzi@wuhuanziMacBook-Air.local>

--- python/sgl_jax/srt/kernels/kda/kda.py
+++ python/sgl_jax/srt/kernels/kda/kda.py
@@ -578,7 +578,6 @@ def _chunk_gated_delta_rule_fwd_kernel(
     bos = seqlens_ref[idx_n]
     eos = seqlens_ref[idx_n + 1]
     real_NT = (eos - bos) // k_ref.shape[2]
-    boh = chunk_offsets_ref[idx_n]
 
     BT = k_ref.shape[2]
     K, V = k_ref.shape[-1], v_ref.shape[-1]
@@ -592,7 +591,7 @@ def _chunk_gated_delta_rule_fwd_kernel(
 
     @pl.when(idx_nt < real_NT)
     def _():
-        h_ref[0, boh + idx_nt, 0] = scratch_ref[...].astype(h_ref.dtype)
+        h_ref[0, 0, 0] = scratch_ref[...].astype(h_ref.dtype)
 
         b_w = w_ref[0, 0]
         b_v = jnp.dot(
@@ -744,6 +743,11 @@ def chunk_gated_delta_rule_fwd_h(
         block_idx = jnp.minimum(bos // BT + nt, T // BT)
         return (0, h, block_idx, 0)
 
+    def _h_index_map(n, h, nt, seqlens_ref, chunk_offsets_ref):
+        bos = pl.multiple_of(seqlens_ref[n], BT)
+        chunk_idx = jnp.minimum(bos // BT + nt, NT - 1)
+        return (0, chunk_idx, h, 0, 0)
+
     k_blockspec = pl.BlockSpec([1, 1, BT, K_PADSIZE], index_map=_t_index_map)
     v_blockspec = pl.BlockSpec([1, 1, BT, V_ALIGNED], index_map=_t_index_map)
     w_blockspec = pl.BlockSpec([1, 1, BT, K_PADSIZE], index_map=_t_index_map)
@@ -759,9 +763,7 @@ def chunk_gated_delta_rule_fwd_h(
         else None
     )
 
-    h_blockspec_out = pl.BlockSpec(
-        [1, NT, 1, K_PADSIZE, V_ALIGNED], index_map=lambda n, h, nt, *_: (0, 0, h, 0, 0)
-    )
+    h_blockspec_out = pl.BlockSpec([1, 1, 1, K_PADSIZE, V_ALIGNED], index_map=_h_index_map)
     v_new_blockspec_out = (
         pl.BlockSpec([1, 1, BT, V_ALIGNED], index_map=_t_index_map) if save_new_value else None
     )
