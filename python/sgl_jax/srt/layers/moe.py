fa8b15c7bafaca7d74919dd79d86d9317d85cc49
Harsh Shah <harsh.shah.hks@gmail.com>
2026-06-08 14:07:00 +0000

    Remove unnecessry update in moe mesh

--- python/sgl_jax/srt/layers/moe.py
+++ python/sgl_jax/srt/layers/moe.py
@@ -89,9 +89,7 @@ class EPMoE(nnx.Module):
 
         abstract_mesh = self.mesh.abstract_mesh
         self.updated_mesh = abstract_mesh.update(
-            axis_sizes=(self.ep_size, self.tp_size),
-            axis_names=("expert", "tensor"),
-            axis_types=(jax.sharding.AxisType.Explicit, jax.sharding.AxisType.Explicit),
+            axis_sizes=(self.ep_size, self.tp_size), axis_names=("expert", "tensor")
         )
 
         with jax.sharding.use_abstract_mesh(self.updated_mesh):
